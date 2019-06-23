a3e443d1bfe18f20078edd16de54c77af1cdf4d7
Faneeza Yassin <faneeza.yassin@gmail.com>
2019-06-23 18:36:04 +0000

    added code to schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,16 @@ ActiveRecord::Schema.define(version: 2019_06_23_154157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "cost"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image"
+    t.index ["user_id"], name: "index_courses_on_user_id"
+  end
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
