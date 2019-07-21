74a4bb6a36dae01fbb3bb6fd12dec68c183a5771
Faneeza Yassin <faneeza.yassin@gmail.com>
2019-07-21 19:11:49 +0000

    carrierwave installed  image_tag undefined error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_183801) do
+ActiveRecord::Schema.define(version: 2019_07_21_182106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_07_20_183801) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
